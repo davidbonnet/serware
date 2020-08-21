ae6099cc40d875bba51839c48ba45dae70d5abf4
David Bonnet <david@bonnet.cc>
2020-08-21 17:08:17 +0200

    fix: fix routeUrl and remove breadcrumbs
    BREAKING CHANGE: `request.breadcrumbs` are removed.
    
    The routeUrl handler now sets the original request `pathname` before
    passing it to the next handler.

--- src/routeUrl.js
+++ src/routeUrl.js
@@ -1,15 +1,16 @@
-import { toPairs, orderBy } from 'lodash'
+import { URL } from 'url'
 
-const { URL } = global
+import { toPairs, orderBy } from 'lodash'
 
 export function routeUrl(routes) {
   const orderedRoutes = orderBy(toPairs(routes), ['0.length'], ['desc'])
   return async function (request, next) {
     if (!request.href) {
-      const href = new URL(`http://${request.headers['host']}${request.url}`)
+      const href = new URL(
+        `http://${request.headers['host'] || 'localhost'}${request.url}`,
+      )
       request.href = href
       request.pathname = href.pathname
-      request.breadcrumbs = []
     }
     const { pathname } = request
     const { length } = orderedRoutes
@@ -17,14 +18,16 @@ export function routeUrl(routes) {
     for (let i = 0; i < length; i++) {
       const route = orderedRoutes[i]
       const pattern = route[0]
-      const middleware = route[1]
+      const handler = route[1]
       if (pattern.length > pathnameLength) {
         continue
       }
       if (pattern === pathname || pathname.startsWith(pattern)) {
         request.pathname = pathname.slice(pattern.length)
-        request.breadcrumbs.push(pattern)
-        return await middleware(request, next)
+        return await handler(request, function (request) {
+          request.pathname = pathname
+          return next(request)
+        })
       }
     }
     return await next(request)

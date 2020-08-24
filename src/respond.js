390964973d0e5616e5bc346bda9609ec4d649089
David Bonnet <david@bonnet.cc>
2020-08-24 15:12:55 +0200

    feat: support status prop in response objects

--- src/respond.js
+++ src/respond.js
@@ -1,9 +1,10 @@
 export function respond(response, options) {
-  const { statusCode, headers, body, charset, cache } = options
+  const { status, statusCode = status, headers, body, charset, cache } = options
   if (statusCode != null) {
     response.statusCode = statusCode
   }
   if (headers != null) {
+    // TODO: Check if headers are iterable
     for (const name in headers) {
       response.setHeader(name, headers[name])
     }

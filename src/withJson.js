06f40275411b0670b5929b66dac8deb8574b04e4
David Bonnet <david@bonnet.cc>
2020-08-30 19:36:31 +0200

    feat: add withJson

---
+++ src/withJson.js
@@ -0,0 +1,10 @@
+export function withJson(value, options = null) {
+  return {
+    ...options,
+    headers: {
+      ...(options != null && options.headers != null ? options.headers : null),
+      'Content-Type': 'application/json',
+    },
+    body: value,
+  }
+}

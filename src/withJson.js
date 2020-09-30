3fd91a04ec85776154e4345999b588bb03beee58
David Bonnet <david@bonnet.cc>
2020-09-30 14:01:00 +0200

    fix(withJson): handle iterable headers in options

--- src/withJson.js
+++ src/withJson.js
@@ -4,9 +4,25 @@ export function withJson(value, options = null) {
   return {
     ...options,
     headers: {
-      ...(options != null && options.headers != null ? options.headers : null),
+      ...(options != null && options.headers != null
+        ? toObject(options.headers)
+        : null),
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(value),
   }
+}
+
+function toObject(headers) {
+  if (typeof headers.entries === 'function') {
+    const result = {}
+    for (const value of headers.entries()) {
+      if (value[1] == null) {
+        continue
+      }
+      result[value[0]] = value[1]
+    }
+    return result
+  }
+  return headers
 }

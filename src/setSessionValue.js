a5d62692e8b06f5efe8888399dd5fb8f2610f2cb
David Bonnet <david@bonnet.cc>
2020-09-30 14:00:41 +0200

    fix(session): omit from session if undefined value

--- src/setSessionValue.js
+++ src/setSessionValue.js
@@ -1,9 +1,15 @@
+import { omit } from 'lodash'
+
 export function setSessionValue(request, name, value) {
   if (!request.session) {
     request.session = {}
   }
   if (request.session[name] === value) {
     return value
+  }
+  if (value === undefined) {
+    request.session = omit(request.session, name)
+    return
   }
   request.session = { ...request.session, [name]: value }
   return value

fe1105aef9316e88de13856bc666cab56c582d17
David Bonnet <david@bonnet.cc>
2021-03-18 12:49:33 +0100

    fix(use): better handle connect API

--- src/use.js
+++ src/use.js
@@ -5,17 +5,28 @@ export function use(handler) {
   return async function (request, next) {
     return new Promise((resolve, reject) => {
       const response = request.respond()
+      let done = false
       const result = handler(request, response, (error) => {
+        done = true
         if (error) {
           reject(error)
           return
         }
         resolve(next(request))
       })
+      if (done) {
+        return
+      }
       if (result != null && typeof result.then === 'function') {
         result.then(() => {
-          resolve(response)
+          if (!done) {
+            resolve(response)
+          }
         })
+        return
+      }
+      if (result !== undefined) {
+        resolve(response)
       }
     })
   }

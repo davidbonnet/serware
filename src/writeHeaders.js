5ff826a9e864ce5cde2b66486ed91c2835a8ea26
David Bonnet <david@bonnet.cc>
2022-05-29 11:41:56 +0200

    chore: add todo

--- src/writeHeaders.js
+++ src/writeHeaders.js
@@ -3,6 +3,7 @@ import { STATUS_CODES } from 'http'
 export async function writeHeaders(request, next) {
   const response = await next(request)
   if (response.headersSent) {
+    // TODO: Raise error?
     return response
   }
   const statusCode = response.statusCode || 200

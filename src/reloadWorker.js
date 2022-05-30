1d0260122c063d6cd5e617de95f8135359c4d9e8
David Bonnet <david@bonnet.cc>
2022-05-30 13:35:52 +0200

    fix(reload): no stack and wrong import
    This is a workaround for module middleware that does not correctly
    import modules by wrapping them in an extra `{ default }` object.

--- src/reloadWorker.js
+++ src/reloadWorker.js
@@ -4,9 +4,16 @@ async function main() {
   const module = await import(modulePath);
   let close;
   try {
-    close = await Promise.resolve(module.default());
+    close = await Promise.resolve(
+      typeof module.default === "function"
+        ? module.default()
+        : module.default?.default &&
+          typeof module.default.default === "function"
+        ? module.default.default()
+        : module(),
+    );
   } catch (error) {
-    respond("error", error.toString());
+    respond("error", `${error.toString()}${error.stack ?? ""}`);
     return;
   }
   respond("success");

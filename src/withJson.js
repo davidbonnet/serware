448420e50843fc47300cced6a2f47f973fc2ccd4
David Bonnet <david@bonnet.cc>
2022-05-30 13:34:23 +0200

    chore: simplify `withJson`

--- src/withJson.js
+++ src/withJson.js
@@ -1,28 +1,7 @@
+import { withContentType } from "./withContentType.js";
+
 const { JSON } = global;
 
-export function withJson(value, options = null) {
-  return {
-    ...options,
-    headers: {
-      ...(options != null && options.headers != null
-        ? toObject(options.headers)
-        : null),
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(value),
-  };
-}
-
-function toObject(headers) {
-  if (typeof headers.entries === "function") {
-    const result = {};
-    for (const value of headers.entries()) {
-      if (value[1] == null) {
-        continue;
-      }
-      result[value[0]] = value[1];
-    }
-    return result;
-  }
-  return headers;
+export function withJson(body, options = null) {
+  return withContentType("text/html", JSON.stringify(body), options);
 }

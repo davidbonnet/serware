4f05eb8a978a6ccf51e75ea35bff3b91bd6ca63b
David Bonnet <david@bonnet.cc>
2023-10-13 11:54:06 +0200

    Adjust import order

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { resolve } from "path";
 
-import moduleList from "vite-plugin-module-list";
-import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { defineConfig } from "vite";
+import moduleList from "vite-plugin-module-list";
 
 export default defineConfig({
   plugins: [

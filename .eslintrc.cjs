d35149d979ce3b77a8a9098a3fe9338d59d21b66
David Bonnet <david@bonnet.cc>
2023-11-05 12:40:17 +0100

    Enforce extensions during import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,6 @@ module.exports = {
   plugins: ["import", "inferno", "tailwindcss"],
   extends: [
     "eslint:recommended",
-    "plugin:import/errors",
     "plugin:import/typescript",
     "preact",
     "prettier",
@@ -58,6 +57,7 @@ module.exports = {
         exceptions: ["a", "b", "x", "y", "i", "j", "_", "io", "id", "fs", "to"],
       },
     ],
+    "import/extensions": ["error", "always", { ignorePackages: true }],
     "import/no-duplicates": "error",
     "import/order": [
       "error",
@@ -73,17 +73,6 @@ module.exports = {
       "error",
       {
         position: "above",
-      },
-    ],
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: [
-          "@material-ui/core/styles",
-          "!@material-ui/core/*",
-          "!@material-ui/icons/*",
-          "!@material-ui/styles/*",
-        ],
       },
     ],
     "no-sequences": "error",

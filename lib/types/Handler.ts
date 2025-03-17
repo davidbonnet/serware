8c20901acc2a49b0e889b8923527029d906410bb
David Bonnet <david@bonnet.cc>
2025-03-17 20:09:22 +0100

    WIP

--- lib/types/Handler.ts
+++ lib/types/Handler.ts
@@ -1,10 +1,6 @@
 import type { Request } from "./Request.ts";
 import type { Response } from "./Response.ts";
 
-// export type Handler =
-//   | ((request: Request) => Response | Promise<Response>)
-//   | ((request: Request, next: Handler) => Response | Promise<Response>);
-
 export interface Handler {
   (request: Request): Response | Promise<Response>;
   (request: Request, next: Handler): Response | Promise<Response>;
